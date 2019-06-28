b248b7bf2ac9002691fa2a9dd76ca2a4e499dc64
Bruce Bigirwenkya <brucebigirwenkya@gmail.com>
2019-06-28 15:48:27 +0300

    Add basic routes

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def healthcheck():
     return 'ok'
+
+@app.route('/movies')
+def movies():
+    return 'list of movies'
+
+@app.route('/movies/<movie_name>')
+def movie(movie_name):
+    return render_template('movie.html', movie_name=movie_name)
