b0c06ad2a8424fc9452e5585a4b078fcd09e835a
Bruce Bigirwenkya <brucebigirwenkya@gmail.com>
2019-07-02 14:23:35 +0300

    Add movies

--- app.py
+++ app.py
@@ -46,28 +46,62 @@ def filterMovies():
 
 @app.route('/movies/add')
 def add_movie():
-    movie = {
-        'title': 'Dark Knight',
-        'description': 'Lorem ipsum',
-        'release_year': 2012,
-        'language_id': 1,
-        'rental_duration': 12,
-        'rental_rate': 2.99,
-        'length': 86,
-        'replacement_cost': 12.99,
-        'rating': 'PG',
-        'last_update': datetime.datetime.now(),
-        'special_features': 'test'
-    }
-    movie['fulltext'] = ', '.join(movie['title'])
-    cur.execute(
-        '''
-        INSERT INTO film (title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating, special_features)
-        VALUES ('{}', '{}', {}, {}, {}, {}, {}, {}, {}, '{}')
-        '''.format(*['a', 'b', 3, 1, 12, 6.99, 7, 8.99, Ratings.PG, 'test'])
-    )
+    lang_id = add_language('English')
+    movies = [
+        {
+            'title': 'Dark Knight',
+            'description': 'Lorem ipsum',
+            'release_year': 2012,
+            'language_id': lang_id,
+            'rental_duration': 12,
+            'rental_rate': 2.99,
+            'length': 86,
+            'replacement_cost': 12.99
+        },
+                {
+            'title': 'Dark Knight 2',
+            'description': 'Lorem ipsum',
+            'release_year': 2012,
+            'language_id': lang_id,
+            'rental_duration': 12,
+            'rental_rate': 2.99,
+            'length': 86,
+            'replacement_cost': 12.99
+        },
+        {
+            'title': 'Dark Knight 3',
+            'description': 'Lorem ipsum',
+            'release_year': 2012,
+            'language_id': lang_id,
+            'rental_duration': 12,
+            'rental_rate': 2.99,
+            'length': 86,
+            'replacement_cost': 12.99
+        },
+    ]
+    for movie in movies:
+        cur.execute(
+            """
+            INSERT INTO film (title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost)
+            VALUES ('{}', '{}', {}, {}, {}, {}, {}, {})
+            """.format(*[v for k, v in movie.items()])
+        )
     try:
         conn.commit()
-        return json.dumps(movie)
+        return json.dumps(movies)
     except Exception as e:
         return 'Error: {}.'.format(e)
+
+def create_ratings(list):
+    cur.execute(f"CREATE TYPE mpaa_ratings as ENUM ('PG', 'G', 'NC-17', 'PG-13')")
+    if conn.commit():
+        return True
+    return False
+
+def add_language(lang):
+    cur.execute(f"INSERT INTO language (name) VALUES ('{lang}')") # add ON CONFLICT (name) DO NOTHING for unique columns
+    cur.execute(f"SELECT language_id FROM language where name='{lang}'")
+    lang_id = cur.fetchone()[0]
+    if conn.commit():
+        return lang_id
+    return lang_id
